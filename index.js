e8cdc177c3c3dfef7867f9a7ad70eb50c2321ec6
yadu nandan <yadunandan4992@gmail.com>
2017-10-28 07:04:49 -0400

    img,title,des

--- index.js
+++ index.js
@@ -19,18 +19,47 @@ function tnews(topic="")
 					var data=$(this);
 					var stream=data.find('.stream-items')
 					var r=[];
+					var urls={}
 					$(stream).children().each(function(idx,ele){
 						var temp={};
 						temp['src']=$(ele).find('.twitter-timeline-link').attr('href');
-						temp['image']=$(ele).find('.AdaptiveMedia-photoContainer').attr('data-image-url');
-						r.push(temp);
+						if (!(temp['src'] in urls))
+						{
+							r.push(temp);	
+						}
 					})
-					resolve(r);
+					delete urls;
+					captureNewsData(r,function(news){
+						resolve(news);
+					})
 				})
 			}
 			else
 			{
 				reject(err);
+			}
+		})
+	})
+}
+function captureNewsData(data,done)
+{
+	var cnt=0
+	data.forEach((item,i)=>{
+		request(item.src,(err,response,html)=>{
+			if(!err)
+			{
+				var $=cheerio.load(html);
+				var title = $('meta[property="og:title"]').attr('content');
+				var description = $('meta[property="og:description"]').attr('content');
+				var image=$('meta[property="og:image"]').attr('content');
+				data[i].title=title;
+				data[i].description=description;
+				data[i].image=image;
+				cnt++;
+				if( cnt==data.length)
+				{
+					done(data);
+				}	
 			}
 		})
 	})
